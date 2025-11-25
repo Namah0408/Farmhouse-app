8aa0b50da4702466e9764a28e75f32ec1f3a5d1f
Namah Agrawal <namahagrawal85@gmail.com>
2025-11-26 01:19:40 +0530

    completed project 6

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -25,9 +25,13 @@ export default function Contact() {
         </div>
 
         <iframe
-          className="w-full h-64 mt-8 rounded-xl"
-          src="https://maps.google.com/maps?q=nagpur&t=&z=13&ie=UTF8&iwloc=&output=embed"
-        />
+        className="w-full h-64 mt-8 rounded-xl"
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3367.887137261922!2d78.76518627525932!3d21.127056080546943!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDA3JzM3LjQiTiA3OMKwNDYnMDMuOSJF!5e1!3m2!1sen!2sin!4v1764099868487!5m2!1sen!2sin"
+        className="w-full h-64 mt-8 rounded-xl"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      />
       </div>
     </div>
   );
